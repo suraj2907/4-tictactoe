21e2c322bed952520ccb48a23ad5abaee1bee1dd
Suraj JAwrani <surajjawrani2022@gmail.com>
2023-06-25 20:59:33 +0530

    setting up states

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
+import React , {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Icon from './Components/icons';
 
-function App() {
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import {Card, CardBody,Container, Button, Col ,Row} from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.css';
+import { useState } from 'react';
+
+const itemArray = new Array(9).fill("empty");
+
+const App = () => {
+
+  const [isCross , setIsCross] = useState(false)
+  const [winMessage , setWinMessage] = useState("")
+  
+  const reloadGame = ()=> {
+    //
+  }
+
+  const checkIsWinner = () => {
+    //
+  }
+
+const changeItem = itemNumber => {
+  //
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Icon />
         <a
           className="App-link"
           href="https://reactjs.org"
